1d1a07bbb08e66e1d078adf910229028b5a8dfe0
achujoseph0709 <achujozefsl0709@gmail.com>
2024-02-19 11:49:14 +0530

    login&otp

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),#Tested
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),#Tested
+    path('send_otp/', SendOTP.as_view(), name='send_otp'),
+    path('verify_otp/', VerifyOTP.as_view(), name='verify_otp'),
    
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,49 @@
+import random
+from django.http import HttpRequest
+import requests
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import OTP
 
-# Create your views here.
+def send_otp(phone_num, otp):
+    print("Reached Otp sent helper")
+    url = "https://www.fast2sms.com/dev/bulkV2"
+    payload = f'variables_values={otp}&route=otp&language=english&numbers={phone_num}'
+    headers = {
+        'authorization': "mEgP0Z5wnldKSerOu1GW8qUbVctH3jkYaM7QCI4Jzp69XNT2ALFmiofRb467D0rSOWVB3qp8J5HYeIvt",
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Cache-Control': "no-cache",
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+ 
+    print(response.text)
+
+def generate_otp():
+    return str(random.randint(1000, 9999))
+
+class SendOTP(APIView):
+    def post(self, request):
+        if 'phone' in request.data:
+            phonenum = request.data['phone']
+            otp = generate_otp()  # Generate OTP
+            # Save OTP to database
+            OTP.objects.create(phone=phonenum, otp=otp)
+            send_otp(phonenum, otp)  # Send OTP (assuming this function is defined)
+            return Response({'message': 'OTP sent successfully'}, status=status.HTTP_200_OK)
+        return Response({'error': 'Phone number is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyOTP(APIView):
+    def post(self, request):
+        if 'phone' in request.data and 'otp' in request.data:
+            phonenum = request.data['phone']
+            user_otp = request.data['otp']
+            try:
+                otp_obj = OTP.objects.get(phone=phonenum, otp=user_otp)
+            except OTP.DoesNotExist:
+                return Response({'error': 'Invalid OTP or phone number'}, status=status.HTTP_400_BAD_REQUEST)
+            otp_obj.delete()
+            return Response({'message': 'OTP verified successfully'}, status=status.HTTP_200_OK)
+        return Response({'error': 'Phone number and OTP are required'}, status=status.HTTP_400_BAD_REQUEST)
+
